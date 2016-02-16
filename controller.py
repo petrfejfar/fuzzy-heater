52afedd5c56102365688220a91f40eaa50e5c6f4
Petr Fejfar <petr_fejfar@cz.ibm.com>
2016-02-16 01:52:17 +0100

    Basic heater simulator finished.

--- controller.py
+++ controller.py
@@ -1,11 +1,24 @@
 #!python3
 import time
+import random
 
 class Controller:
     # driver is class of
     def runController(self, driver):
         # do very basic work
+        power = 0.0; # ratio 
+        desired_temp = 33.0; # Celsius
         while(True):
-            print("Temperature on thermometer 0 is ", driver.temperature(0), "°C");
+            temp1 = driver.temperature(0);
+            temp2 = driver.temperature(1);
+            avg_temp = (temp1 + temp2) / 2.0;
+            
+            if(avg_temp < desired_temp):
+                power = min(1.0, power + 0.02);
+            else:
+                power = max(0.0, power - 0.02);                
+            driver.heat(power);
+                    
+            print("Temperature of system is ", avg_temp, "°C and controller is set to ", power);
             time.sleep(2);
         return;
--- test.py
+++ test.py
@@ -3,11 +3,25 @@ import random
 import threading
 import controller
 import time;
+import matplotlib.pyplot as plt
+import datetime
 
-class SimulatedHeater:
+class SimulatedHeater:    
+    # enviroment constants
+    water_volume = 0.25; # liters
+    water_ther_capacity = 4185.5; # J/(kg*K)
+    heater_power = 25; # Watt
+    enviroment_temp = 23.0; # Celsius
+    
+    # model
+    actual_temp = 23.0; # Celsius
+    power_ratio = 0.0; # ratio
+
+    # tuples (date, heat set)
+    heat_log = [];
+
     lock = threading.Lock();
-    actual_temp = 25;
-
+    
     def __init__(self):
         self.__run();
         
@@ -19,14 +33,17 @@ class SimulatedHeater:
             if(index != 0 and index != 1):
                 raise Exception("Invalid thermometer index.");
         
-        return self.actual_temp;
+        return self.actual_temp + random.random() - 0.5;
         
     # ratio is interval 0.0 to 1.0 
     # 0.0 means turn off
     # 1.0 means turn on
     # <0.0; 1.0> is linear proportion
-    def power(self, ratio):
+    def heat(self, ratio):
         with self.lock:
+            self.power_ratio = ratio;
+            self.heat_log.append((datetime.datetime.now(), ratio));        
+                
             if(ratio < 0.0 or ratio > 1.0):
                 raise Exception("Argument ratio out of range.");
         
@@ -37,15 +54,49 @@ class SimulatedHeater:
         # update inner state of model       
         def update_state():
             while True:
-                self.actual_temp += random.random() - 0.5;
-                time.sleep(0.02);
+                with self.lock:
+                    time_span = 0.02; # sec
+                    
+                    # simulate heat from heater 
+                    energy_emitted = self.power_ratio * self.heater_power * time_span; # J
+                    therm_delta = energy_emitted / (self.water_ther_capacity * self.water_volume); # Celsius
+                    self.actual_temp += therm_delta;
+                    
+                    # simulate enviroment cooling (magic aproximation)
+                    energy_emitted = 0.5 * self.heater_power * time_span; # J
+                    therm_delta = energy_emitted / (self.water_ther_capacity * self.water_volume); # Celsius
+                    self.actual_temp -= therm_delta;
+                    
+                time.sleep(time_span);
             return;
             
         # draw plot with historical data about model
         def make_plot():
-            # TODO
-            return;            
-        
+            temp1_log = [];           
+            temp2_log = [];           
+            while True:
+                    
+                temp1_log.append((datetime.datetime.now(), self.temperature(0)));                
+                temp2_log.append((datetime.datetime.now(), self.temperature(1)));                
+                
+                # draw
+                plt.clf();    
+                plt.title('Temperature on sensors');
+                [val, date] = zip(*temp1_log);
+                plt.subplot(211);
+                plt.plot(val, date, color = "red", aa = True, label = "temp1");
+                [val, date] = zip(*temp2_log);
+                plt.plot(val, date, color = "green", aa = True, label = "temp2");
+                if self.heat_log:
+                    with self.lock:
+                        plt.title('Heat commands');
+                        [val, date] = zip(*self.heat_log);
+                        plt.subplot(212)
+                        plt.plot(val, date, color = "blue", aa = True, label = "heat");
+                plt.savefig("history.png");
+                
+                time.sleep(2);
+           
         thread = threading.Thread(target=update_state);
         # thread dies on program exit
         thread.daemon = True;  
