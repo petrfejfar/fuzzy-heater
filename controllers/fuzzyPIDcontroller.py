62cba82c98e249645a9cf98d6044afa1c0f8c8be
Petr Fejfar <petr_fejfar@cz.ibm.com>
2016-03-06 13:40:09 +0100

    License added.

--- controllers/fuzzyPIDcontroller.py
+++ controllers/fuzzyPIDcontroller.py
@@ -19,25 +19,3 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-import random
-
-# from controllers.RPidriver import RPiDriver
-from controllers.simulationdriver import SimulationDriver
-
-from controllers.dummycontroller import DummyController
-from controllers.PIDcontroller import PIDController
-from controllers.fuzzycontroller import FuzzyController
-
-if(__name__ == "__main__"):
-    print("Simulation start.")
-    try:
-        driver = SimulationDriver()
-        # driver = RPiDriver()
-        # c = DummyController()
-        c = PIDController()
-        c.runController(driver)
-    finally:
-        driver.close()
-
-    print("Simulation ends.")
